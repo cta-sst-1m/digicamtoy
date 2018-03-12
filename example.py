6e89295bb82dd29879f5045e0c29f05f99d64d1b
Dominik Neise <neised@phys.ethz.ch>
2018-03-12 20:15:28 +0100

    pep8

--- example.py
+++ example.py
@@ -5,15 +5,19 @@ import numpy as np
 
 if __name__ == '__main__':
 
-    toy = NTraceGenerator(pulse_shape_file='/utils/pulse_SST-1M_pixel_0.dat',
-                          n_pixels=3, baseline=[0, 500, 0],
-                          n_photon=[[100, 133], [60, 60, 60], [100, 133]],
-                          time_signal=[[0, 100], [20, 50, 100], [0, 100]],
-                          gain=[10, 10, 29])
+    toy = NTraceGenerator(
+        pulse_shape_file='/utils/pulse_SST-1M_pixel_0.dat',
+        n_pixels=3,
+        baseline=[0, 500, 0],
+        n_photon=[[100, 133], [60, 60, 60], [100, 133]],
+        time_signal=[[0, 100], [20, 50, 100], [0, 100]],
+        gain=[10, 10, 29])
 
-    t = np.arange(toy.time_start + toy.artificial_backward_time,
-                  toy.time_end,
-                  toy.time_sampling)
+    t = np.arange(
+        toy.time_start + toy.artificial_backward_time,
+        toy.time_end,
+        toy.time_sampling
+    )
 
     for event in toy:
 
