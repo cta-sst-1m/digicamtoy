f5e1c23d0280113aa7dec347504d9a4de3c87744
Cyril Alispach <cyril.alispach@unige.ch>
2018-03-19 10:09:11 +0100

    creating job scripts

--- run.sh
+++ run.sh
@@ -6,20 +6,20 @@
 #SBATCH --ntasks=1
 #SBATCH --time=0-12:00:00
 #SBATCH --partition=mono
-#SBATCH --output=slurm-%A.out
-#SBATCH --error=slurm-%A.err
+#SBATCH --output=$HOME/output/slurm-%A.out
+#SBATCH --error=$HOME/output/slurm-%A.err
 #SBATCH --mail-user=cyril.alispach@unige.ch --mail-type=END
 #SBATCH --mem=2000
 #SBATCH --verbose
 
 
-echo "-----START JOB at `date`-----"
+echo "-----START JOB $SLURM_JOB_ID at `date`-----"
 
-module load intel/2017a Python/3.5
+# module load intel/2017a Python/3.5
 srun source /home/alispach/.anaconda3/envs/digicamtoy/bin/activate
 srun python produce_data.py -y config_files/commissioning/ac_$1_dc_$2_id_$3.yml
 
 
-echo "-----END   JOB at `date`-----"
+echo "-----END $SLURM_JOB_ID JOB at `date`-----"
 
 # EOF
