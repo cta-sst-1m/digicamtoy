096f7cbfc8edb71e1d97eebe7050c5f25d0c99f9
Cyril Alispach <cyril.alispach@unige.ch>
2018-03-19 11:16:21 +0100

    fix activate

--- jobs.sh
+++ jobs.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-N_AC=5
-N_DC=30
-N_FILES=100
 
-for (( i=0;i<$N_AC;i++)); do
-    for (( j=0;j<$N_DC;j++)); do
-        for (( k=0;k<$N_FILES;k++)); do
+AC_START=0
+DC_START=1
+FILE_ID_START=0
+AC_END=29
+DC_END=1
+FILE_ID_END=199
+
+for (( i=$AC_START;i<=$AC_END;i++)); do
+    for (( j=$DC_START;j<=$DC_END;j++)); do
+        for (( k=$FILE_ID_START;k<=$FILE_ID_END;k++)); do
         sbatch run.sh ${i} ${j} ${k}
         done
     done
--- run.sh
+++ run.sh
@@ -6,19 +6,19 @@
 #SBATCH --ntasks=1
 #SBATCH --time=0-12:00:00
 #SBATCH --partition=mono
-#SBATCH --output=$HOME/output/slurm-%A.out
-#SBATCH --error=$HOME/output/slurm-%A.err
+# SBATCH --export=HOME
+#SBATCH --output=/home/alispach/output/slurm-%A.out
+#SBATCH --error=/home/alispach/output/slurm-%A.err
 #SBATCH --mail-user=alispach
 #SBATCH --mail-type=END
 #SBATCH --mem=2000
 #SBATCH --verbose
-# SBATCH --export=HOME
 
 
 echo "-----START JOB $SLURM_JOB_ID at `date`-----"
 
-# module load intel/2017a Python/3.5
-srun source $HOME/.anaconda3/envs/digicamtoy/bin/activate
+# srun source /home/alispach/.anaconda3/envs/digicamtoy/bin/activate
+source /home/alispach/.anaconda3/envs/digicamtoy/bin/activate digicamtoy
 srun python produce_data.py -y config_files/commissioning/ac_$1_dc_$2_id_$3.yml
 
 
