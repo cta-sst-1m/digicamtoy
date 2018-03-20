c2bbabe13346ac0fed8250390355013cb4f930b7
Cyril Alispach <cyril.alispach@unige.ch>
2018-03-20 13:19:44 +0100

    update sbatch sub. script

--- run.sh
+++ run.sh
@@ -10,15 +10,15 @@
 #SBATCH --error=/home/alispach/output/slurm-%A.err
 #SBATCH --mail-user=alispach
 #SBATCH --mail-type=END
-#SBATCH --mem=2000
+#SBATCH --mem=1000 # 8000
 #SBATCH --verbose
 
 echo "-----START JOB $SLURM_JOB_ID at `date`-----"
 
 # srun source /home/alispach/.anaconda3/envs/digicamtoy/bin/activate
 source $HOME/.anaconda3/envs/digicamtoy/bin/activate digicamtoy
-srun python produce_data.py -y config_files/commissioning/ac_$1_dc_$2_id_$3.yml
-
+# srun python produce_data.py -y config_files/commissioning/ac_$1_dc_$2_id_$3.yml
+python generate.py
 
 echo "-----END JOB $SLURM_JOB_ID at `date`-----"
 
