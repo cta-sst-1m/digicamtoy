e6b70cf8bd5a69a6e6549f8ef28b14399802feb0
Dominik Neise <neised@phys.ethz.ch>
2018-04-07 13:04:21 +0200

    copy parameter list from NTraceGen

--- produce_data.py
+++ produce_data.py
@@ -10,27 +10,29 @@ from commandr import command, Run, SetOptions
 
 @command('main')
 def produce_data(
-    outfile_path,
+    out_path,
     n_events=100,
-    n_photon=0,
-    nsb_rate=0.003,
-    n_pixels=1296,
     time_start=0,
-    time_end=368,
+    time_end=200,
     time_sampling=4,
+    n_pixels=1296,
+    nsb_rate=0.003,
     crosstalk=0.08,
     gain_nsb=True,
+    n_photon=0,
     poisson=True,
+    sigma_e=0.8,
     sigma_1=0.8,
     gain=5.8,
     baseline=200,
-    time_signal=0.,
+    time_signal=20,
     jitter=0.3,
+    sub_binning=0
 ):
     n_bins = (time_end - time_start) // time_sampling
     _locals = locals()
 
-    hdf5 = h5py.File(outfile_path, 'w')
+    hdf5 = h5py.File(out_path, 'w')
     hdf5.attrs.update(_locals)
     hdf5.attrs['date'] = str(datetime.datetime.now())
 
