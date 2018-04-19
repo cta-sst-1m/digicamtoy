c21652f65988892ba95a84b032d998dca4cc6094
Dominik Neise <neised@phys.ethz.ch>
2018-04-19 15:03:15 +0200

    reduce nsb_rate in this test to make sure this is running fast

--- digicamtoy/test/test_NTraceGenerator.py
+++ digicamtoy/test/test_NTraceGenerator.py
@@ -5,6 +5,9 @@ def test_n_events():
 
     test_n_events = [0, 1, 10, 100]
     for n_events in test_n_events:
-        toy = NTraceGenerator(n_events=n_events)
+        toy = NTraceGenerator(
+            nsb_rate=0.000001,  # small rate for faster tests
+            n_events=n_events
+        )
         events = [e for e in toy]
         assert len(events) == n_events
