daa08591e3ef8982f93529574d7a396558145dac
Cyril Alispach <cyril.alispach@unige.ch>
2018-10-15 10:45:53 +0200

    some speed tests show that it is not very effective

--- digicamtoy/utils/pdf.py
+++ digicamtoy/utils/pdf.py
@@ -1,6 +1,7 @@
 import numpy as np
-from scipy.special import factorial, loggamma, gammaln
+from scipy.special import factorial, gammaln
 from scipy.stats import rv_discrete
+import time
 
 
 class Borel(rv_discrete):
@@ -11,7 +12,16 @@ class Borel(rv_discrete):
 
     def _pmf(self, k, mu_xt):
 
-        return np.exp(self.logpmf(k, mu_xt))
+        x = mu_xt * k
+        prob = np.exp(- x)
+        prob *= x ** (k - 1)
+        prob /= factorial(k)
+
+        return
+
+    # def _pmf(self, k, mu_xt):
+
+    #    return np.exp(self.logpmf(k, mu_xt))
 
     def logpmf(self, k, mu_xt):
 
@@ -36,29 +46,64 @@ if __name__ == '__main__':
 
     import matplotlib.pyplot as plt
 
-    mu = np.array([0.2, 0.1])[0]
-    mu_xt = np.array([0.08, 0.1])[0]
+    n_pixels = 3
+    n_photons = 1000
 
+    mu = np.array([1])[0]
+    mu_xt = np.linspace(0.0001, 1, num=n_pixels, endpoint=False)
     borel = Borel()
-    data = borel.rvs(mu_xt=mu_xt, size=(10000, ))
+
+    t0 = time.time()
+
+    data = borel.rvs(mu_xt=mu_xt, size=(n_photons, len(mu_xt))).T
+
+    print(time.time() - t0)
+
+    t0 = time.time()
+
+    for i in range(n_pixels):
+
+        borel.rvs(mu_xt[i], size=(n_photons, ))
+
+    print(time.time() - t0)
 
     bins = np.arange(data.min(), data.max() + 1, 1)
 
     plt.figure()
-    plt.hist(data, bins=bins, normed=True)
-    plt.plot(bins, borel.pmf(bins, mu_xt=mu_xt))
-    plt.plot(bins, np.exp(borel.logpmf(bins, mu_xt=np.array([mu_xt, mu_xt]))))
+
+    for i, m in enumerate(mu_xt):
+        label = '$\mu_{XT} =$' + ' {}'.format(m)
+
+        plt.hist(data[i], bins=bins, normed=True, alpha=0.3,
+                 label=label)
+
+        mean = np.mean(data[i])
+        label += '\n Mean : {}'.format(mean)
+        plt.axvline(mean, label=label)
+        # plt.plot(bins, borel.pmf(bins, mu_xt=mu_xt[i]))
+        # plt.plot(bins, np.exp(borel.logpmf(bins, mu_xt=mu_xt[i])))
+
+    plt.legend(loc='best')
 
     generalized_poisson = GeneralizedPoisson()
     data = generalized_poisson.rvs(mu=mu,
-                                   mu_xt=mu_xt, size=(10000,))
+                                   mu_xt=mu_xt, size=(n_photons, len(mu_xt)))
 
     bins = np.arange(data.min(), data.max() + 1, 1)
 
     plt.figure()
-    plt.hist(data, bins=bins, normed=True)
-    plt.plot(bins, generalized_poisson.pmf(bins, mu=mu, mu_xt=mu_xt))
+
+    for i, m in enumerate(mu_xt):
+
+        label = '$\mu_{XT} =$' + ' {}'.format(m)
+        plt.hist(data[:, i], bins=bins, normed=True, alpha=0.3,
+                     label=label)
+        mean = np.mean(data[:, i])
+        label += '\n Mean : {}'.format(mean)
+        plt.axvline(mean, label=label)
+        # plt.plot(bins, generalized_poisson.pmf(bins, mu=mu, mu_xt=mu_xt[0]))
     # plt.plot(bins, np.exp(borel.logpmf(bins, mu_xt=mu_xt)))
 
+    plt.legend()
     plt.show()
 
